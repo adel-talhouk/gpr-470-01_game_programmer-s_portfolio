fa7fb5be0dcd99a1994c768db0ddd09a921bb2d7
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2022-02-17 00:41:25 -0500

    Added comment explaining the purpose of BaseWeapon.

--- FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapons/BaseWeapon.cs
+++ FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapons/BaseWeapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//DERIVE FROM THIS CLASS for each weapon type. This allows the player to hold multiple weapons and to have different firign modes (full-auto, semi-auto)
 public abstract class BaseWeapon : MonoBehaviour
 {
     //Fire
