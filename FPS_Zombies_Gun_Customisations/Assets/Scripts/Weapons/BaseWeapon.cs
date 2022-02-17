c7011aff6462f18075822da209b3695ae43b8ac1
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2022-02-17 00:42:08 -0500

    Updated comment.

--- FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapons/BaseWeapon.cs
+++ FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapons/BaseWeapon.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//DERIVE FROM THIS CLASS for each weapon type. This allows the player to hold multiple weapons and to have different firign modes (full-auto, semi-auto)
+//DERIVE FROM THIS CLASS for each weapon type. This allows the player to hold multiple weapons and to have different firing modes (full-auto, semi-auto)
+//This will also allow for a weapon manager to have references to the BaseWeapon and have access to derived classes for easy switching.
 public abstract class BaseWeapon : MonoBehaviour
 {
     //Fire
