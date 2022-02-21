0746cd4f718dfb69dd0bad556b93294ec878c769
Adel Talhouk <adel.talhouk@mymail.champlain.edu>
2022-02-21 10:39:56 -0500

    Added more variables to weapon attachements.

--- FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapon_Attachements/BaseWeaponGrip.cs
+++ FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapon_Attachements/BaseWeaponGrip.cs
@@ -7,4 +7,7 @@ public class BaseWeaponGrip : ScriptableObject
 {
     //Amount of recoil
     [Range(0.1f, 1.0f)] public float recoil;
+
+    //Aim Down Sights speed
+    [Range(1.0f, 2.0f)] public float adsSpeedMultiplier;
 }
--- FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapon_Attachements/BaseWeaponStock.cs
+++ FPS_Zombies_Gun_Customisations/Assets/Scripts/Weapon_Attachements/BaseWeaponStock.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "WeaponAttachments/Stock")]
 public class BaseWeaponStock : ScriptableObject
 {
-
+    //Amount of weapon sway
+    [Range(0.1f, 1.0f)] public float weaponSway;
 }
